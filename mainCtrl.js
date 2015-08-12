7315b75d595ac34ecda22e43333f6d4e54f840c0
Jason Turner <jasonalmaturner@gmail.com>
2015-08-11 20:12:04 -0600

    adding ng-options

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,19 +1,35 @@
 var app = angular.module('sdk');
 
 app.controller('mainCtrl', function($scope, mainService){
-  // $scope.getBirds = function(){
-  //   mainService.getBirds($scope.order, $scope.status).then(function(res){
-  //     $scope.birds = res;
-  //   }, function(err){
-  //     console.log(err);
-  //   });
-  // };
+  $scope.err = false;
+  $scope.IUCNCategories = ["all", "lc", "nt", "vu", "en", "cr", "cr (pe)", "cr (pew)", "ew", "ex", "dd", "nr", "ur"]
 
-  $scope.getBirds = function(){
+  $scope.getBirdsSansSDK = function(){
+    if($scope.status === 'all'){
+      $scope.status = false;
+    };
+    mainService.getBirds($scope.order, $scope.status).then(function(res){
+      $scope.birds = res;
+      $scope.err = false;
+    }, function(err){
+      console.log(err);
+      $scope.err = 'No birds found';
+      $scope.birds = [];
+    });
+  };
+
+  $scope.getBirdsSDK = function(){
+    if($scope.status === 'all'){
+      $scope.status = false;
+    };
+    console.log($scope.status)
     mainService.mySDK($scope.order, $scope.status, function(err, birds){
       if(err){
         console.log(err);
+        $scope.err = 'No birds found';
+        $scope.birds = [];
       } else {
+        $scope.err = false;
         $scope.birds = birds;
       };
     });
--- mainService.js
+++ mainService.js
@@ -3,10 +3,14 @@ var app = angular.module('sdk');
 app.service('mainService', function($http, $q){
 
   this.getBirds = function(order, status){
+    var category = status ? 'IUCNRedListCategory2014=' + status : '';
+    var theOrder = order ? 'order=' + order : '';
+    var and = status && order ? '&' : '';
+    var url = 'http://bird-api.com/api/birds?' + theOrder + and + category
     var dfd = $q.defer();
     $http({
       method: 'GET',
-      url: 'http://bird-api.com/api/birds?order=' + order + '&IUCNRedListCategory2014=' + status
+      url: url
     }).then(function(res){
       dfd.resolve(res.data);
     }, function(err){
@@ -16,9 +20,13 @@ app.service('mainService', function($http, $q){
   };
 
   this.mySDK = function(order, status, cb){
+    var category = status ? 'IUCNRedListCategory2014=' + status : '';
+    var theOrder = order ? 'order=' + order : '';
+    var and = status && order ? '&' : '';
+    var url = 'http://bird-api.com/api/birds?' + theOrder + and + category
     $http({
       method: 'GET',
-      url: 'http://bird-api.com/api/birds?order=' + order + '&IUCNRedListCategory2014=' + status
+      url: url
     }).then(function(res){
       cb(null, res.data);
     }, function(err){
